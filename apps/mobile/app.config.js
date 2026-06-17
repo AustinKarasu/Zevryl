1eb881474894274a20a515acc24330ffea7b8d54
Zevryl Development <dev@zevryl.app>
2026-06-17 18:06:09 +0530

    Use runner Java for APK release

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -2,7 +2,7 @@ const app = {
   expo: {
     name: 'Zevryl',
     slug: 'zevryl',
-    version: '0.1.5',
+    version: '0.1.6',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'dark',
